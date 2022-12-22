c22e111c623269c499b171c736a0cb39b4ea2355
Daydream0929 <2352869723@qq.com>
2022-12-22 13:59:58 +0800

    完成均匀交叉和变异

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <vector>
+#include <numeric>
+#include <time.h>
+#include <stdlib.h>
 
 using namespace std;
 
@@ -7,13 +10,13 @@ const int population = 20;   // 种群规模
 const int n = 30;           // n的长度
 const int iteration = 100;  // 代数限制
 const double muteProb = 0.01;  // 变异概率
-const double crossProb = 0.99;  // 交叉概率
+const double crossProb = 0.50;  // 交叉概率
 
-vector<vector<int>> parents(n, vector<int>(population));  // 父代
+vector<vector<int>> parents(population, vector<int>(n));  // 父代
+vector<vector<int>> children(population, vector<int>(n));  // 子代
 vector<int> maxFitness(iteration);  // 种群中每一代的最大适应度
 vector<int> avgFitness(iteration);  // 种群中每一代的平均适应度
 vector<int> fitness(population);  // 种群中每个个体的适应度
-vector<double> probability(population);  // 根据个体适应度
 
 int cal_fitness(int i) {    //计算第i个种群的适应度，在这里返回1的个数即可
     int res = 0;
@@ -31,16 +34,6 @@ void update_fitness() {  // 更新种群中所有个体的适应度
     }
 }
 
-void update_probability() {  //轮盘赌更新每个个体的概率
-    int sum = 0;
-    for (auto fit : fitness) {
-        sum += fit;
-    }
-    for (int i = 0; i < population; i ++) {
-        probability[i] = (double)fitness[i] * 1.0 / sum;
-    }
-}
-
 void init() {  //初始化种群
     for (int i = 0; i < population; i ++ ) {
         for (int j = 0; j < n; j ++ ) {
@@ -49,22 +42,84 @@ void init() {  //初始化种群
     }
 }
 
-int main() {
-    init();  // 初始化种群
+pair<int, int> select() {  // 选择
+    pair<int, int> par;
+    int a, b;
+    int sum = accumulate(fitness.begin(), fitness.end(), 0);
+    double t1 = 1.0 * sum * (rand() / (double)RAND_MAX);
+    double t2 = 1.0 * sum * (rand() / (double)RAND_MAX);
+    int t = 0;
+    for (int i = 0; i < fitness.size(); i ++ ) {
+        t += fitness[i];
+        if (t1 <= t) {
+            a = i;
+            break;
+        }
+    }
+    t = 0;
+    for (int i = 0; i < fitness.size(); i ++ ) {
+        t += fitness[i];
+        if (t2 <= t) {
+            b = i;
+            break;
+        }
+    }
+    return {a, b};
+}
 
+pair<vector<int>, vector<int>> crossover(int p1, int p2) {  // 均匀交叉
+    vector<int> c1 = parents[p1], c2 = parents[p2];
+    for (int i = 0; i < n; i ++ ) {
+        double t = rand() / (double) RAND_MAX;
+        if (t < crossProb) {
+            swap(c1[i], c2[i]);
+        }
+    }
+    return {c1, c2};
+}
+
+void mutation(vector<int>&c) {  // 变异
+    for (int i = 0; i < n; i ++ ) {
+        double t = rand() / (double) RAND_MAX;
+        if (t < muteProb) {
+            c[i] ^= 1;
+        }
+    }
+}
+
+void update_parents() { // 更新父代
+    parents = children;
+    update_fitness();
+}
+
+int main() {
+    srand((unsigned)time(NULL));  // 设置随机数种子
+    init();  // 初始化种群
     int generation = 0;
     while (generation <= iteration) {
         generation ++;  // 迭代次数 + 1
 
         update_fitness();  // 计算父代的适应度
-        update_probability();  // 根据父代的适应度更新轮盘赌概率
 
-        vector<vector<int>> children(n, vector<int>(population));  // 定义子代
+        for (int i = 0; i < population / 2; i ++ ) {
+            auto p = select();  // select
+
+            auto c = crossover(p.first, p.second);  // crossover
+
+            mutation(c.first);  // mutation
+            mutation(c.second);
+
+            children[i * 2] = c.first;
+            children[i * 2 + 1] = c.second;
+
+            update_parents();  //  update
+        }
+
         for (int i = 0; i < population; i ++ ) {
-            cout << probability[i] << ' ';
+            cout << fitness[i] << " ";
         }
         cout << endl;
-
     }
+
     return 0;
 }
